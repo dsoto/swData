e69fdbeb18fa86f0d1659d2d12f4219d21209d9d
Daniel Soto <danielrsoto@gmail.com>
2009-01-28 10:50:58 -0800

    updated analyzeNCAll.m with cell array headers

--- 20090127-sws7-nc/analyzeNCAll.m
+++ 20090127-sws7-nc/analyzeNCAll.m
@@ -1,6 +1,6 @@
-% analyzeNCAll.m
+% analyzeSBAll.m
 % loops through all files in ./data/ and calls
-% analyzeNC.m on each .data file
+% analyzeSB.m on each .data file
 
 
 % FIXME : put this in function for use in all scripts
@@ -16,38 +16,32 @@ numFiles = length(listFiles);
 
 % open data file for logging of files and parameters
 % to create index of data
-% write header columns 
-logFileHandle = fopen('20081118-nc.data','w');
-% line 1
-fprintf(logFileHandle,'% 15s\t','Sample');
-fprintf(logFileHandle,'% 15s\t','Cantilever');
-fprintf(logFileHandle,'% 15s\t','Cantilever');
-fprintf(logFileHandle,'% 15s\t','Stage');
-fprintf(logFileHandle,'% 15s\t','Preload');
-fprintf(logFileHandle,'% 15s\t','Microwedge');
-fprintf(logFileHandle,'% 15s\t','Spring');
-fprintf(logFileHandle,'\n');
-% line 2
-fprintf(logFileHandle,'\t');
-fprintf(logFileHandle,'\t');
-fprintf(logFileHandle,'% 15s\t','Deflection');
-fprintf(logFileHandle,'% 15s\t','Preload');
-fprintf(logFileHandle,'% 15s\t','Force');
-fprintf(logFileHandle,'% 15s\t','Deflection');
-fprintf(logFileHandle,'% 15s\t','Constant');
-fprintf(logFileHandle,'\n');
-% line 3
-fprintf(logFileHandle,'\t');
-fprintf(logFileHandle,'\t');
-fprintf(logFileHandle,'% 15s\t','(um)');
-fprintf(logFileHandle,'% 15s\t','(um)');
-fprintf(logFileHandle,'% 15s\t','(uN)');
-fprintf(logFileHandle,'% 15s\t','(um)');
-fprintf(logFileHandle,'% 15s\t','(N/m)');
-fprintf(logFileHandle,'\n');
+logFileHandle = fopen('20090127-sws7-nc.data','w');
 
-% loop through all files in directory
-% and call shearBendingAnalysis.m
+formatString20 = '% 20s\t';
+formatString15 = '% 15s\t';
+
+% columnHeaders{column} = {line1,line2,line3,formatstring}
+columnHeaders{1} = {'Data',       'File',      'Name',    formatString20};
+columnHeaders{2} = {'Trajectory', 'File',      'Name',    formatString20};
+columnHeaders{3} = {'Sample',     '',          ''    ,    formatString15};
+columnHeaders{4} = {'Cantilever', '',          ''    ,    formatString15};
+columnHeaders{5} = {'Cantilever', 'Deflection','(um)',   formatString15};
+columnHeaders{6} = {'Stage',      'Preload',   '(um)',    formatString15};
+columnHeaders{7} = {'Preload',    'Force',     '(uN)',    formatString15};
+columnHeaders{8} = {'Microwedge', 'Deflection','(um)',    formatString15};
+columnHeaders{9} = {'Spring',     'Constant',  '(N/m)',    formatString15};
+
+% loop through cell array to construct headers
+for i = 1:3			% line loop
+	for j = 1:9		% column loop
+		% indexed by columnHeaders{column}(line)
+		fprintf(logFileHandle,char(columnHeaders{j}(4)),char(columnHeaders{j}(i)));
+	end
+	fprintf(logFileHandle,'\n');
+end
+
+% loop through data files and call analyzeLS.m
 for i = 1:numFiles
 	fileName = ['./data/',listFiles(i).name];
 	fprintf('Processing File %d of %d : % 10s\n',i,numFiles,fileName);
