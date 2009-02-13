fd60b6dadf7733b2396df7aff398725b29450d8e
Daniel Soto <danielrsoto@gmail.com>
2009-02-13 13:55:01 -0800

    20090211-sws10-st added raw plot output these scripts only output the voltage signal and make no scalings or calculations. also added plots

---
+++ 20090211-sws10-st/plotRawForceTraceAll.m
@@ -0,0 +1,28 @@
+% analyzeLSAll.m
+% loops through all files in ./data/ and calls
+% analyzeLS.m on each .data file
+
+
+% FIXME : put this in function for use in all scripts
+% function getDataFiles();
+% go into data directory
+
+pathString = './data/';
+
+cd(pathString);
+% grab list of files
+listFiles = dir('*.data');
+% go back to parent directory
+cd('..');
+
+% count files
+numFiles = length(listFiles);
+
+for i = 1:numFiles
+	fileName = [pathString,listFiles(i).name];
+	fprintf('Processing File %d of %d : % 10s\n',i,numFiles,fileName);
+	figure = gcf;
+	axes = gca;
+	plotRawForceTrace(fileName,figure,axes);
+	close(figure);
+end
