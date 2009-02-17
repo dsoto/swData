c5975a3d57b72c94a65c0c575f96a5c99f189c8b
Daniel Soto <danielrsoto@gmail.com>
2009-02-17 13:29:23 -0800

    pythonDev - tidying up

--- pythonDev/plotRawForceTraceAll.py
+++ pythonDev/plotRawForceTraceAll.py
@@ -6,5 +6,7 @@ import glob
 fileNameList = glob.glob('*.data')
 for fileName in fileNameList:
 	roxanne.plotDataFile(fileName)
+	roxanne.plotDataFileChaco(fileName)
+	roxanne.plotDataFileChacoPNG(fileName)
 
 
