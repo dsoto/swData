64d4e1d19cc7afd32dd1f72045c9169fb1a2774e
Daniel Soto <danielrsoto@gmail.com>
2009-02-18 19:25:18 -0800

    20090211-sws10-st - decent first cut at analyzeSlantTest.py most of the python is ironed out i still need to verify that the analysis is correct

--- 20090211-sws10-st/analyzeSlantTest.py
+++ 20090211-sws10-st/analyzeSlantTest.py
@@ -8,7 +8,6 @@ def addToDict(kvDict, key, tempLine):
 	return kvDict			
 
 def readDataFile(fileName):
-	print 'readDataFile ' + fileName
 	# read in file
 	fileIn = open(fileName,'r')
 	# read in lines until find data tag
@@ -24,17 +23,16 @@ def readDataFile(fileName):
 		if tempLine.find('normal') != -1:
 			key = 'norAmp'
 			kvDict = addToDict(kvDict, key, tempLine)
-# 			index = tempLine.find('=')+1
-# 			length = len(tempLine)
-# 			val = tempLine[index-length:]
-# 			kvDict.update({key:val})			
 		if tempLine.find('lateral') != -1:
 			key = 'latAmp'
 			kvDict = addToDict(kvDict, key, tempLine)
-# 			index = tempLine.find('=')+1
-# 			length = len(tempLine)
-# 			val = tempLine[index-length:]
-# 			kvDict.update({key:val})
+		if tempLine.find('roll') != -1:
+			key = 'rollAngle'
+			kvDict = addToDict(kvDict, key, tempLine)
+		if tempLine.find('pitch') != -1:
+			key = 'pitchAngle'
+			kvDict = addToDict(kvDict, key, tempLine)
+
 		if tempLine.find('data starts here')!=-1:
 			keepReading = 0
 		
@@ -60,9 +58,125 @@ def readDataFile(fileName):
 	fileIn.close()
 	return [strings,column1,column2,column3,column4,kvDict]
 
-list=readDataFile('data/st_sws10_20090211_145840.data')
-kvDict = list[5]
-print kvDict
-latAmp = kvDict['latAmp']
-latAmp = float(latAmp)
-print latAmp
+def readIndexFile(fileName):
+#	print 'readIndexFile ' + fileName
+	fileIn = open(fileName,'r')
+	# toss first three lines
+	for i in range(3):
+		tempLine = fileIn.readline()
+	tempData = fileIn.readlines()
+	# initialize data arrays
+	fileNames = []
+	indexContact = []
+	indexPreload = []
+	indexAdhesion = []
+	# loop through data and append arrays
+	for line in tempData:
+		line=line.replace('\n','')
+		value=line.split('\t')
+		fileNames.append(value[0])
+		indexContact.append(int(value[1]))
+		indexPreload.append(int(value[2]))
+		indexAdhesion.append(int(value[3]))
+	# tidy up and return values
+	fileIn.close()
+	return [fileNames,indexContact,indexPreload,indexAdhesion]
+
+def main():
+	
+	import glob
+	import os.path
+	import numpy
+	
+	indexList = readIndexFile('parsed.data')
+	
+	fOut = open('test.data','w')
+
+		
+	fileNameList = glob.glob('st_sws10_*.data')
+
+	for fileName in fileNameList:
+		dataList = readDataFile(fileName)
+
+		lateralVoltage        = numpy.array(dataList[1])
+		normalVoltage         = numpy.array(dataList[2])
+		positionNormalMicron  = dataList[3] * 10
+		positionLateralMicron = dataList[4] * 10
+						
+		normalStiffness = 1.0
+		lateralStiffness = 1.0
+		normalDisplacement = 1.0
+		lateralDisplacement = 1.0
+		lateralAmplification = 1.0
+		normalAmplification = 1.0
+
+		defaultAmplification = 100
+		lateralDisplacement = (lateralDisplacement * lateralAmplification /
+													 defaultAmplification)
+		normalDisplacement = (normalDisplacement * normalAmplification /
+ 												 defaultAmplification)
+ 												 
+		# use cantilever values to convert voltages to forces
+		lateralForceMicroNewton = -1 * lateralVoltage * lateralStiffness / lateralDisplacement
+		normalForceMicroNewton = normalVoltage * normalStiffness / normalDisplacement
+
+		print len(lateralForceMicroNewton)
+
+		fileName = os.path.splitext(fileName)
+		fileName = fileName[0]
+		fileNames = indexList[0]
+		indexFileName = fileNames.index(fileName)	
+
+		indexContact     = indexList[1][indexFileName]
+		indexMaxPreload  = indexList[2][indexFileName]
+		indexMaxAdhesion = indexList[3][indexFileName]
+
+#		print indexContact
+#		print indexMaxPreload
+#		print indexMaxAdhesion
+
+		# find maximum (negative) adhesion value; this is pulloff
+		maxAdhesionUncompensatedMicroNewton = normalForceMicroNewton[indexContact]
+		# store shear value corresponding to max adhesion
+		maxShearUncompensatedMicroNewton = lateralForceMicroNewton[indexMaxAdhesion]
+		# back up and find maximum normal value; this is max preload
+		maxPreloadMicroNewton = normalForceMicroNewton[indexMaxAdhesion]
+		# back up and find min normal value; this is point of contact
+		normalForceContactMicroNewton = normalForceMicroNewton[indexContact]
+		# find corresponding value of contact for shear
+		shearForceContactMicroNewton = lateralForceMicroNewton[indexContact]
+
+		normalCantileverVoltageMaxAdhesion = normalVoltage[indexMaxAdhesion]
+		# get stage displacement at max adhesion
+		normalStagePositionMaxAdhesion = positionNormalMicron[indexMaxAdhesion]
+		# get cantilever deflection at contact
+		normalCantileverVoltageContact = normalVoltage[indexContact]
+		# get stage displacement at contact
+		normalStagePositionContact = positionNormalMicron[indexContact]
+		# calculate effective stage preload
+		normalStagePreload = (normalStagePositionMaxAdhesion - 
+													normalStagePositionContact)
+		# calculate effective cantilever deflection
+		normalCantileverDeflection = (normalCantileverVoltageContact - 
+																	normalCantileverVoltageMaxAdhesion)/normalDisplacement
+		# calculate effective microwedge deflection (effective preload)
+		effectivePreload = normalCantileverDeflection + normalStagePreload
+		# adhesion force = maxAdhesion - force at contact
+		maxAdhesionMicroNewton = (maxAdhesionUncompensatedMicroNewton - 
+															normalForceContactMicroNewton)
+		# shear force = maxShear - force at contact
+		maxShearMicroNewton = (maxShearUncompensatedMicroNewton - 
+													 shearForceContactMicroNewton)
+
+		print indexContact
+#		input()
+		fOut.write(fileName + '\t')
+		fOut.write(repr(effectivePreload) + '\t')
+		fOut.write(repr(maxAdhesionMicroNewton) + '\t')
+		fOut.write(repr(maxShearMicroNewton))
+		fOut.write('\n')
+	
+	fOut.close()
+
+if __name__ == '__main__':
+	main()
