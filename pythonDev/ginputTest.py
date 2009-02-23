a74ddd59cd2322f0f8270f3390ddcc84ed5a3550
Daniel Soto <danielrsoto@gmail.com>
2009-02-22 17:32:47 -0800

    pythonDev - trying more basic test of ginput

--- pythonDev/ginputTest.py
+++ pythonDev/ginputTest.py
@@ -1,9 +1,28 @@
 #!/usr/bin/env python
 
-import roxanne
-import glob
+import pylab
 
-fileNameList = glob.glob('*.data')
-for fileName in fileNameList:
-	roxanne.getContactPoint(fileName)
+x = pylab.arange(0,10,0.1)
+y = pylab.sin(x)
+
+for i in range(1,2):
+	print i
+	plotname = 'ginputTest'+repr(i)+'.pdf'
+	# plot and get user input, then close fig
+	pylab.figure(1)
+	pylab.plot(x,y)
+	points = pylab.ginput(i)
+	points = pylab.array(points)
+	pylab.close(1)
+	
+	
+	points[:,1] = pylab.sin(points[:,0])
+	
+	pylab.figure(2)
+	# replot with chosen points
+	pylab.plot(x,y)
+	pylab.plot(points[:,0],points[:,1],'ko')
+	
+	pylab.savefig(plotname)
+	pylab.close(2)
 
