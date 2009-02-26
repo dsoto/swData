83871f2714cc649691a941dd0142a461c94751fe
Daniel Soto <danielrsoto@gmail.com>
2009-02-26 09:50:17 -0800

    20090225-sws10-ls - finished plot script for limit surface and generated limit surface for sws10 this limit surface is back to the JKR--frictional adhesion limit surface

---
+++ 20090225-sws10-ls/plotLS.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+# plotting script that expects a header line with labels for 
+# each column of data
+# these columns of data will be assigned to a dictionary
+# returned values are all strings
+
+import roxanne
+
+def main():
+	fileName = 'analyzed.data'
+	fileIn = open(fileName,'r')
+	columnDict = roxanne.readDataFileArray(fileIn)
+	
+	shearForce = columnDict['forceMaxShear']
+	normalForce = columnDict['forceMaxAdhesion']
+	
+	import matplotlib
+	params = {'font.family': 'serif',
+	          'font.serif' : 'Computer Modern Roman',
+	          'text.usetex': True}
+	matplotlib.rcParams.update(params)
+	
+	import matplotlib.pyplot
+	import matplotlib.axis
+	
+	
+	matplotlib.pyplot.plot(shearForce,normalForce,
+									       linestyle = 'None',
+									       marker = 'o',
+	                       markerfacecolor = 'w',
+	                       markeredgecolor = 'g')
+	matplotlib.pyplot.xlabel('Shear Force (microNewtons)')
+	matplotlib.pyplot.ylabel('Adhesion Force (microNewtons)')
+	matplotlib.pyplot.title('sws10 - 529b02 - Limit Surface - 20090225')
+	matplotlib.pyplot.grid(True)
+	matplotlib.pyplot.axis([0, 10, -5, 0])
+	matplotlib.pyplot.savefig('mplLimitSurface.pdf',transparent=True)
+#	matplotlib.pyplot.show()
+
+	import os
+	os.system('open mplLimitSurface.pdf')
+
+# TODO : format plot with fonts and size
+
+if __name__ == '__main__':
+	main()
+
+
