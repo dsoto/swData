adf961db5fd735f51da2536fc810d5d7794ef7ba
Daniel Soto <danielrsoto@gmail.com>
2009-05-22 16:49:30 -0700

    changed plotDataFileMPLPDF to reflect newer methods such as no longer using matplotlib.pyplot.plot but using figure and axes objects using file to dictionary methods for the data reading

--- roxanne/roxanne.py
+++ roxanne/roxanne.py
@@ -151,54 +151,39 @@ def plotDataFileChacoPNG(fileName):
 
 def plotDataFileMPLPDF(fileName):
     import os.path
-    import matplotlib.pyplot
+    import matplotlib.pyplot as plt
 
     baseFileName = os.path.splitext(fileName)
     baseFileName = baseFileName[0]
+    print 'reading file ',fileName
     plotFileName = baseFileName + '.pdf'
-    dataArray = readDataFile(fileName)
-    time = dataArray[0]
-    voltageNormal = dataArray[1]
-    voltageShear = dataArray[2]
-    positionX = dataArray[3]
-    positionY = dataArray[4]
 
-    matplotlib.pyplot.plot(voltageNormal)
-    matplotlib.pyplot.hold(True)
-    matplotlib.pyplot.plot(voltageShear)
-    matplotlib.pyplot.hold(False)
-    matplotlib.pyplot.xlabel('Time (ms)')
-    matplotlib.pyplot.ylabel('Piezo Voltage Signal (V)')
-    matplotlib.pyplot.title(plotFileName)
-    matplotlib.pyplot.savefig(plotFileName,transparent=True)
-    matplotlib.pyplot.close()
-    return 0
-
-def readDataFile(fileName):
-    # read in file
     fileIn = open(fileName,'r')
-    # read and discard first 9 lines of file
-    for i in range(9):
-        tempLine=fileIn.readline()
-    tempData = fileIn.readlines()
-    # initialize data arrays
-    strings=[]
-    column1=[]
-    column2=[]
-    column3=[]
-    column4=[]
-    # loop through data and append arrays
-    for line in tempData:
-        line=line.replace('\n','')
-        value=line.split('\t')
-        strings.append(value[0])
-        column1.append(float(value[1]))
-        column2.append(float(value[2]))
-        column3.append(float(value[3]))
-        column4.append(float(value[4]))
-    # tidy up and return values
-    fileIn.close()
-    return [strings,column1,column2,column3,column4]
+    header = readDataFileHeader(fileIn)
+    dataArray = readDataFileArray(fileIn)
+
+    time = dataArray['time']
+    #print time
+    # send this array of strings to get a proper time
+    voltageNormal = map(float,dataArray['voltageForceNormal'])
+    voltageLateral = map(float,dataArray['voltageForceLateral'])
+    positionX = map(float,dataArray['voltagePositionX'])
+    positionY = map(float,dataArray['voltagePositionY'])
+
+    figure = plt.figure()
+    axes = figure.add_subplot(111)
+    
+    # have to turn off TeX if i want to print the filename
+    axes.plot(voltageNormal, label='normal voltage')
+    axes.plot(voltageLateral, label='lateral voltage')
+    axes.plot(positionX, label='x position')
+    axes.plot(positionY, label='y position')
+    axes.set_xlabel('Sample Number')
+    axes.set_ylabel('Piezo Voltage Signal (V)')
+    #axes.set_title(plotFileName)
+    axes.legend()
+    
+    figure.savefig(plotFileName,transparent=True)
 
 def addToDict(kvDict, key, tempLine):
     index = tempLine.find('=')+1
@@ -1187,6 +1172,33 @@ class SimpleZoom(AbstractOverlay, ToolHistoryMixin, BaseZoomTool):
                 del state[key]
 
         return state
+
+# deprecated methods below
+def readDataFile(fileName):
+    # read in file
+    fileIn = open(fileName,'r')
+    # read and discard first 9 lines of file
+    for i in range(9):
+        tempLine=fileIn.readline()
+    tempData = fileIn.readlines()
+    # initialize data arrays
+    strings=[]
+    column1=[]
+    column2=[]
+    column3=[]
+    column4=[]
+    # loop through data and append arrays
+    for line in tempData:
+        line=line.replace('\n','')
+        value=line.split('\t')
+        strings.append(value[0])
+        column1.append(float(value[1]))
+        column2.append(float(value[2]))
+        column3.append(float(value[3]))
+        column4.append(float(value[4]))
+    # tidy up and return values
+    fileIn.close()
+    return [strings,column1,column2,column3,column4]
 
 if __name__ == '__main__':
     main()
