5006efb44896501dc617e0a77fdcb1fdf2e8df1f
Daniel Soto <danielrsoto@gmail.com>
2009-11-17 11:56:41 -0800

    20090602-sws10-sls - adding tons more files

---
+++ 20090602-sws10-sls/data/separated/plots/plotRawForceTraceAll.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import sys
+sys.path.append('/Users/dsoto/current/swDataFlat/roxanne')
+import roxanne
+import glob
+
+fileNameList = glob.glob('*.data')
+for fileName in fileNameList:
+	roxanne.plotDataFileMPLPDF(fileName)
+#	roxanne.plotDataFileChacoPDF(fileName)
+#	roxanne.plotDataFileChacoPNG(fileName)
+
+
