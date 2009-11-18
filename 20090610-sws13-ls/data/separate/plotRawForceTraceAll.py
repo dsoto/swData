cfb62560eca8e7f94adf2e0f90c55d41b6553318
Daniel Soto <danielrsoto@gmail.com>
2009-11-18 11:49:43 -0800

    20090610-sws13-ls - adding force raw plot call

--- 20090610-sws13-ls/data/separate/plotRawForceTraceAll.py
+++ 20090610-sws13-ls/data/separate/plotRawForceTraceAll.py
@@ -5,7 +5,8 @@ sys.path.append('/Users/dsoto/current/swDataFlat/roxanne')
 import roxanne
 import glob
 
-fileNameList = glob.glob('*.data')
+fileNameList = glob.glob('p3*.data')
 
 for fileName in fileNameList:
-	roxanne.plotDataFileMPLPDF(fileName)
+#	roxanne.plotDataFileMPLPDF(fileName)
+    roxanne.plotDataFileForce(fileName)
