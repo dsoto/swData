ad8e31be625a86a79d1ac65e520a5ff75ff2386b
Daniel Soto <danielrsoto@gmail.com>
2009-11-25 14:24:48 -0800

    20091124-sws11-ls - adding separated data files adding separateData.sh batch shell script

---
+++ code/separateData.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+./separateData.py ../20091124-sws11-ls/data/raw/sws11-ls-20091124_140600.data ../20091124-sws11-ls/traj/LS_p30_d80_vp20_vd20.index p30_
+./separateData.py ../20091124-sws11-ls/data/raw/sws11-ls-20091124_140904.data ../20091124-sws11-ls/traj/LS_p33_d80_vp20_vd20.index p33_
+./separateData.py ../20091124-sws11-ls/data/raw/sws11-ls-20091124_141152.data ../20091124-sws11-ls/traj/LS_p36_d80_vp20_vd20.index p36_
+
+
