56e4c6b16306478c354f257fc426c0457448f927
Daniel Soto <danielrsoto@gmail.com>
2010-03-25 15:55:58 -0700

    code - moved convert data file function into roxanne and tested on test data

--- code/convertDataFile.py
+++ code/convertDataFile.py
@@ -4,85 +4,8 @@
 import roxanne as rx
 import numpy as np
 
-# this function should be in roxanne
-def getHeader(fileIn):
-    # loop through header and return header in dictionary
-    # if line has equals sign put lhs of equals as string for dict key
-    # and put rhs as value
-    header = {}
-    keepReading = 1
-    while keepReading == 1:
-        tempLine = fileIn.readline()
-        if tempLine.find('=') != -1:
-            (key,value) = tempLine.split('=')
-            key = key.strip()
-            value = value.strip()
-            header[key] = value
-        if tempLine.find('<data>') != -1:
-            keepReading = 0
-    return header
 
-def outputDictAsText(dict, fileOut):
-    for key in dict.keys():
-        fileOut.write(key + ' = ' + str(dict[key])+ '\n')
-
-def outputDictAsTextByIndex(dict, i, fileOut):
-    newDict = {}
-    for key in dict.keys():
-        newDict[key]=dict[key][i]
-    outputDictAsText(newDict, fileOut)
-
-def convertTime(timeStrings):
-    timeStrings = data['time']
-    timeSeconds = [float(ts[0:2])*3600+float(ts[3:5])*60+float(ts[6:]) for ts in timeStrings]
-    timeSeconds = np.array(timeSeconds)
-    timeSeconds = timeSeconds - timeSeconds[0]
-    timeSeconds = map(str,timeSeconds)
-    return timeSeconds
-
-def convertData(data, cantileverDict):
-    # convert time to list of floats
-    data['time'] = convertTime(data['time'])
-
-    # convert all lists to numpy arrays for ease of manipulation
-    #keys = data.keys()
-    for key in data.keys():
-        data[key] = np.array(data[key],dtype=float)
-
-
-    # make conversions
-    data['positionLateralMicron'] = data['voltagePositionY']*10.0
-    data['positionNormalMicron']  = data['voltagePositionX']*10.0
-
-    # cantilever stiffnesses in newtons per meter
-    normalStiffness      = cantileverDict['normalStiffness']
-    lateralStiffness     = cantileverDict['lateralStiffness']
-    # displacement sensitivity measured at 100x amplification
-    normalDisplacement   = cantileverDict['normalDisplacement']
-    lateralDisplacement  = cantileverDict['lateralDisplacement']
-
-    # use cantilever values to convert voltages to forces
-    data['forceLateralMicroNewton'] = (data['voltageForceLateral'] *
-                                       lateralStiffness / lateralDisplacement)
-    data['forceNormalMicroNewton']  = (data['voltageForceNormal'] *
-                                       normalStiffness / normalDisplacement)
-    # make polarity adjustments
-    data['forceLateralMicroNewton'] *= -1
-
-    return data
-
-
-'''input stuff'''
-# open data file
 rawFile = open('/Users/dsoto/current/swDataFlat/999-test-data/data/raw/test.data','r')
-# read header, rip into dictionary
-rawHeaderDict = getHeader(rawFile)
-# grab data
-data = rx.readDataFileArray(rawFile)
-# need to get cantilever values, micron conversion, and polarity stuff
-# convert according to rx.cantilever and sign conventions
-cantileverDict = rx.getCantileverData(rawHeaderDict['cantilever'])
-data = convertData(data,cantileverDict)
 
 '''output stuff'''
 fileOutName = '/Users/dsoto/current/swDataFlat/999-test-data/data/separated/outFile.data'
@@ -90,27 +13,5 @@ print fileOutName,
 print 'opening',
 fileOut = open(fileOutName,'w')
 print 'writing',
-outputDictAsText(rawHeaderDict, fileOut)
-fileOut.write('<data>\n')
-# output header
-# output data list
-startIndex = 1
-endIndex = len(data['time'])
-keys = data.keys()
-keys.sort()
-colWidth = max(map(len, keys)) + 1
-for key in keys:
-    fileOut.write(key.ljust(colWidth))
-fileOut.write('\n')
-# get starting time
-startTime = data['time'][startIndex]
-for j in range(startIndex, endIndex):
-    for key in keys:
-        if key == 'time':
-            fileOut.write(str(data[key][j]-startTime).ljust(colWidth))
-        else:
-            fileOut.write(str(data[key][j]).ljust(colWidth))
-    fileOut.write('\n')
-print 'finishing'
 
-
+rx.convertDataFile(rawFile, fileOut)
--- code/roxanne.py
+++ code/roxanne.py
@@ -9,6 +9,8 @@ from enthought.enable.api import ColorTrait, KeySpec
 from enthought.traits.api \
     import Bool, Enum, Float, Instance, Int, Str, Trait, Tuple
 
+import numpy as np
+
 def main():
     print 'roxanne.py'
     return 0
@@ -469,6 +471,23 @@ def getCantileverData(cantilever):
 
     return cantileverDict
 
+def getHeader(fileIn):
+    # loop through header and return header in dictionary
+    # if line has equals sign put lhs of equals as string for dict key
+    # and put rhs as value
+    header = {}
+    keepReading = 1
+    while keepReading == 1:
+        tempLine = fileIn.readline()
+        if tempLine.find('=') != -1:
+            (key,value) = tempLine.split('=')
+            key = key.strip()
+            value = value.strip()
+            header[key] = value
+        if tempLine.find('<data>') != -1:
+            keepReading = 0
+    return header
+
 def readDataFileHeader(fileIn):
     # read in file
     # read in lines until find data tag
@@ -598,6 +617,93 @@ def getTimeStamp():
     from datetime import datetime
     dt = datetime.now()
     return dt.strftime('%Y%m%d_%H%M')
+
+def outputDictAsText(dict, fileOut):
+    for key in dict.keys():
+        fileOut.write(key + ' = ' + str(dict[key])+ '\n')
+
+def outputDictAsTextByIndex(dict, i, fileOut):
+    newDict = {}
+    for key in dict.keys():
+        newDict[key]=dict[key][i]
+    outputDictAsText(newDict, fileOut)
+
+def convertTime(timeStrings):
+    #timeStrings = data['time']
+    timeSeconds = [float(ts[0:2])*3600+float(ts[3:5])*60+float(ts[6:]) for ts in timeStrings]
+    timeSeconds = np.array(timeSeconds)
+    timeSeconds = timeSeconds - timeSeconds[0]
+    timeSeconds = map(str,timeSeconds)
+    return timeSeconds
+
+def convertData(data, cantileverDict):
+    # convert time to list of floats
+    data['time'] = convertTime(data['time'])
+
+    # convert all lists to numpy arrays for ease of manipulation
+    #keys = data.keys()
+    for key in data.keys():
+        data[key] = np.array(data[key],dtype=float)
+
+
+    # make conversions
+    data['positionLateralMicron'] = data['voltagePositionY']*10.0
+    data['positionNormalMicron']  = data['voltagePositionX']*10.0
+
+    # cantilever stiffnesses in newtons per meter
+    normalStiffness      = cantileverDict['normalStiffness']
+    lateralStiffness     = cantileverDict['lateralStiffness']
+    # displacement sensitivity measured at 100x amplification
+    normalDisplacement   = cantileverDict['normalDisplacement']
+    lateralDisplacement  = cantileverDict['lateralDisplacement']
+
+    # use cantilever values to convert voltages to forces
+    data['forceLateralMicroNewton'] = (data['voltageForceLateral'] *
+                                       lateralStiffness / lateralDisplacement)
+    data['forceNormalMicroNewton']  = (data['voltageForceNormal'] *
+                                       normalStiffness / normalDisplacement)
+    # make polarity adjustments
+    data['forceLateralMicroNewton'] *= -1
+
+    return data
+
+def convertDataFile(rawFile,fileOut):
+    '''input stuff'''
+    # open data file
+    # read header, rip into dictionary
+    rawHeaderDict = getHeader(rawFile)
+    # grab data
+    data = readDataFileArray(rawFile)
+    # need to get cantilever values, micron conversion, and polarity stuff
+    # convert according to rx.cantilever and sign conventions
+    cantileverDict = getCantileverData(rawHeaderDict['cantilever'])
+    data = convertData(data,cantileverDict)
+
+    outputDictAsText(rawHeaderDict, fileOut)
+    fileOut.write('<data>\n')
+    # output header
+    # output data list
+    startIndex = 1
+    endIndex = len(data['time'])
+    keys = data.keys()
+    keys.sort()
+    colWidth = max(map(len, keys)) + 1
+    for key in keys:
+        fileOut.write(key.ljust(colWidth))
+    fileOut.write('\n')
+    # get starting time
+    startTime = data['time'][startIndex]
+    for j in range(startIndex, endIndex):
+        for key in keys:
+            if key == 'time':
+                fileOut.write(str(data[key][j]-startTime).ljust(colWidth))
+            else:
+                fileOut.write(str(data[key][j]).ljust(colWidth))
+        fileOut.write('\n')
+    print 'finishing'
+
+
+
 
 # numpy import must be outside to work
 from numpy import allclose, inf
