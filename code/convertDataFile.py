9ea9cf91fa68e2ae01d3957688c82b8c35404850
Daniel Soto <danielrsoto@gmail.com>
2010-03-25 18:39:30 -0700

    code - fixed convertDataFile to glob the .data files in a directory and loop through them and convert them

--- code/convertDataFile.py
+++ code/convertDataFile.py
@@ -3,15 +3,28 @@
 # file to separate data file and convert to force and micron data
 import roxanne as rx
 import numpy as np
+import os.path
+import glob
 
+directory = '../035-sws17-length/'
+fileNameList = glob.glob(directory + 'data/raw/*.data')
 
-rawFile = open('/Users/dsoto/current/swDataFlat/999-test-data/data/raw/test.data','r')
+for rawFileName in fileNameList:
+    rawFile = open(rawFileName, 'r')
+    #rawFile = open('~/current/swDataFlat/999-test-data/data/raw/test.data','r')
 
-'''output stuff'''
-fileOutName = '/Users/dsoto/current/swDataFlat/999-test-data/data/separated/outFile.data'
-print fileOutName,
-print 'opening',
-fileOut = open(fileOutName,'w')
-print 'writing',
+    # get full path name of input file
+    fullRawFilePath = os.path.abspath(rawFileName)
+    # separate path and filename
+    tempPath, fileName = os.path.split(fullRawFilePath)
+    tempPath = os.path.split(tempPath)[0]
+    outPath = os.path.join(tempPath, 'separated')
+    outFileName = os.path.join(outPath, fileName)
 
-rx.convertDataFile(rawFile, fileOut)
+    '''output stuff'''
+    print outFileName,
+    print 'opening',
+    fileOut = open(outFileName,'w')
+    print 'writing',
+
+    rx.convertDataFile(rawFile, fileOut)
